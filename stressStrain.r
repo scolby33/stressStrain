c8f7b60859ab656903dee466a2fbb1ff65342167
Scott Colby <scott@scolby.com>
2014-11-13 16:03:18 -0500

    Renamed and commented
    Renamed scripts to reflect repository name.
    Added comments and usage information to the R script and the shell
    script, respectively.

---
+++ stressStrain.r
@@ -0,0 +1,38 @@
+#!/usr/bin/Rscript
+
+args = commandArgs(TRUE)
+rawData = read.csv(args[1], stringsAsFactors=FALSE)	# args[1] = data.csv
+
+force = as.numeric(rawData[-1, "Load"])	# extract the relevant data as numeric
+extension = as.numeric(rawData[-1, "Extension"]) # extension = current length - initial length
+xsection = as.numeric(rawData[2, "xsection"])
+startLen = as.numeric(rawData[2, "startLen"])
+
+stress = force / xsection
+strain = extension / startLen
+
+# independant variable: strain
+# dependant variable: stress
+
+quartz()
+plot(strain, stress, main = args[1], type = "l")	# plot the stress-strain curve
+range = identify(strain, stress, n = 2)	# and allow user to select linear area
+garbage = dev.off()	# hide the output of dev.off()
+
+elasticStress = stress[range[1]:range[2]]	# the data points in the selected range
+elasticStrain = strain[range[1]:range[2]]	# are from the elastic region of the curve
+
+lmFit = lm(elasticStress ~ elasticStrain)
+sink(paste(getwd(),"/output/", sub(".csv", "", args[1]), "-summary.txt", sep="")) # output/data-summary.txt
+summary(lmFit)
+sink()
+
+pdf(paste(getwd(),"/output/", sub(".csv", "", args[1]), ".pdf", sep = "")) # output/data.pdf
+plot(strain, stress, main = args[1], sub = paste("Range:", range[1], "to", range[2]), type = "l")	# plot the whole curve
+points(elasticStrain, elasticStress, type = "l", col = "red")	# highlight the selected area
+abline(lmFit, col = "blue")	# show the fitted line
+garbage = dev.off()
+
+pdf(paste(getwd(),"/output/", sub(".csv", "", args[1]), "-diag.pdf", sep=""))	# output/data-diag.pdf
+plot(lmFit)	# save residuals, etc.
+garbage = dev.off()
---
+++ stressStrain.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# stressStrain.sh
+# Scott Colby - 2014
+
+## Usage: stressStrain.sh <path>
+
+shopt -s nullglob	# make sure "*.csv" expands to null if no csv's are present
+
+if [ $# -eq 0 ]
+then
+	echo "usage: $0 <path>"
+	echo "Passes each .csv file in <path> to stresStrain.r, then moves the processed"
+	echo "data file to <path>/done once complete"
+	exit 1
+fi
+
+cd $1
+
+mkdir -p output	# set up directory structure
+mkdir -p done
+
+if [ -n "$(echo *.csv)" ]
+then
+	for f in *.csv	# process each data file with R script
+	do
+		echo "Procesing $f"
+		./stress-strain.r $f
+		mv $f done/
+	done
+else
+	echo "    No .csv files in folder $1."
+	exit 1
+fi
+
+exit 0
